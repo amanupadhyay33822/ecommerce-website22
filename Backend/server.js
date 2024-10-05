6359fde7b1c8ddcbc80c20258d104ba295d1d5d0
QuadbCompany <neeraja.naru@quadbtech.com>
2024-10-05 12:51:20 +0530

    Debugging deployment

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 // const orderRoutes = require('./routes/order');
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { connect } = require("./db/dbconfig");
+//const { connect } = require("./db/dbconfig");
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
@@ -39,6 +39,6 @@ app.get('/', (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running `);
 });
 module.exports = app
