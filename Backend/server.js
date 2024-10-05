b1b98ec5aa638afbfd3860971de340b3bfeb47d9
QuadbCompany <neeraja.naru@quadbtech.com>
2024-10-05 12:36:28 +0530

    Modified env

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const { connect } = require("./db/dbconfig");
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = 8081;
 
 
 //middlewares
