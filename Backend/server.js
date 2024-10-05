22b39f684c2df3103b6a60ce6486374a7ee3908f
QuadbCompany <neeraja.naru@quadbtech.com>
2024-10-05 14:44:14 +0530

    Error while login and signup

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const { connect } = require("./db/dbconfig");
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 5000
 
 
 //middlewares
@@ -38,6 +39,6 @@ app.use("/order", orderRoutes);
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}/api/v1`);
 });
 module.exports = app
